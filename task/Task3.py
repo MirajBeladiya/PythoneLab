87c57e9168328d97d58bfb12cc15095f0b694302
MirajBeladiya <mirajbeladiya@gmail.com>
2024-08-28 16:05:27 +0530

    Initial commit

---
+++ task/Task3.py
@@ -0,0 +1,21 @@
+#1 Difference between = and == operators in Python
+# The operator is used for assigning values to variables.
+# For example: x = 5 assigns the v The operator is used for comparing if two values are equal.
+# For example: x == 5 checks
+
+#2 What does the ** operator do in Python?
+
+# The** operator is used for exponentiation in Python.
+# It raises the number on the left to 2** 3 evaluates to 8 (2 raised to the power of 3) x** 2 squares the value of x
+
+#3 What does the operator do in Python?
+
+# The operator in Python performs a bitwise XOR operation on the operands. It compares eac
+
+# The operator is commonly used in contexts like:
+
+# Cryptography: For encrypting and decrypting data using XOR operations
+
+# Bit manipulation: For performing operations on individual bits of a number
+
+# Checking for unique elements in a list or set
